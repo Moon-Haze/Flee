f8a178dc7d13b5dd2dfee141cae3b083e2bcec73
Moon-Haze <swx1126200515@outlook.com>
2023-02-25 09:25:31 +0800

    修改log

--- test/Flee_test.cpp
+++ test/Flee_test.cpp
@@ -2,11 +2,12 @@
  * @Author: Moon-Haze swx1126200515@outlook.com
  * @Date: 2023-01-24 20:42
  * @LastEditors: Moon-Haze swx1126200515@outlook.com
- * @LastEditTime: 2023-02-14 17:39
+ * @LastEditTime: 2023-02-25 09:24
  * @FilePath: \Flee\test\Flee_test.cpp
  * @Description:
  */
 
+#include "log.h"
 #include <climits>
 #include <cstddef>
 #include <cstdint>
@@ -30,5 +31,6 @@ int main(int argc, char** argv) {
     std::ofstream stream(std::string("test.json"));
     jsonWriter->write(root, &stream);
     stream.close();
+    Flee_Debug << "root=";
     return 0;
 }
